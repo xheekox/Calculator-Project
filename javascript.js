118e6179ed7445bfc3f471ea0c07579c6456d330
Ronald Perez <rpere034@gmail.com>
2023-07-08 14:32:07 -0600

    created initial calculator javascript functions and variables

--- javascript.js
+++ javascript.js
@@ -17,3 +17,24 @@ const divide = function(a, b)
 {
     return a / b;
 }
+
+const operate = function(operator, a, b)
+{
+    if(operator === "+")
+    {
+        return add(a, b);
+    } else if (operator === "-")
+    {
+        return subtract(a, b);
+    } else if (operator === "x")
+    {
+        return multiply(a, b);
+    } else 
+    {
+        return divide(a, b);
+    }
+}
+
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
