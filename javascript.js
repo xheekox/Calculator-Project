22ec50e44693fb6ff34224180d629c543deaad89
Ronald Perez <rpere034@gmail.com>
2023-07-08 19:34:47 -0600

    completed intial logic for calculator

--- javascript.js
+++ javascript.js
@@ -38,3 +38,35 @@ const operate = function(operator, a, b)
 let firstNumber = 0;
 let secondNumber = 0;
 let operator = "";
+
+// add event listeners to buttons
+const displayButton = function(button)
+{
+    const display = document.querySelector(".display");
+
+    console.log(button);
+    if(button.srcElement.id === "clearBtn")
+    {
+        display.textContent = "";
+    } else if (button.srcElement.className === "numberBtn")
+    {
+        display.textContent += button.srcElement.textContent;
+    } else if (button.srcElement.className === "equalsBtn")
+    {
+        secondNumber = parseInt(display.textContent);
+        display.textContent = operate(operator, firstNumber, secondNumber);
+    } else
+    {
+        operator = button.srcElement.textContent;
+        firstNumber = parseInt(display.textContent);
+        display.textContent = "";
+    }
+}
+
+const Buttons = document.querySelectorAll('button');
+
+
+Buttons.forEach((button) =>
+{
+    button.addEventListener('click', displayButton);
+});
