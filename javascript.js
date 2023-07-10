da7f0c250e11fb860f2ba4448f7de57108865194
Ronald Perez <rpere034@gmail.com>
2023-07-09 21:49:45 -0600

    completed rev1 of calculator

--- javascript.js
+++ javascript.js
@@ -38,31 +38,66 @@ const operate = function(operator, a, b)
 let firstNumber = 0;
 let secondNumber = 0;
 let operator = "";
+let initialNumPress = true;
 
 // add event listeners to buttons
 const displayButton = function(button)
 {
     const display = document.querySelector(".display");
 
-    console.log(button);
+    console.log(firstNumber);
     if(button.srcElement.id === "clearBtn")
     {
         display.textContent = "";
+        firstNumber = 0;
+        secondNumber = 0;
+        initialNumPress = true;
     } else if (button.srcElement.className === "numberBtn")
     {
+        if(initialNumPress) 
+        {
+            display.textContent = "";
+            initialNumPress = false;
+        }
+
         display.textContent += button.srcElement.textContent;
+        initialNumPress = false;
+
     } else if (button.srcElement.className === "equalsBtn")
     {
-        secondNumber = parseInt(display.textContent);
-        display.textContent = operate(operator, firstNumber, secondNumber);
+        if(firstNumber != 0)
+        {
+            secondNumber = parseInt(display.textContent);
+            display.textContent = operate(operator, firstNumber, secondNumber);
+            firstNumber = parseInt(display.textContent);
+            secondNumber = 0;        
+        }
+
+        initialNumPress = true;
+
     } else
     {
+        if ( secondNumber === 0)
+        {
+            secondNumber = parseInt(display.textContent);
+            if(operator === "") 
+            {
+                operator = button.srcElement.textContent;
+            } 
+
+            display.textContent = operate(operator, firstNumber, secondNumber);
+            firstNumber = parseInt(display.textContent);
+            secondNumber = 0;
+        
+        } 
+        
         operator = button.srcElement.textContent;
-        firstNumber = parseInt(display.textContent);
-        display.textContent = "";
+        initialNumPress = true;
+
     }
 }
 
+document.querySelector(".display").textContent = "";
 const Buttons = document.querySelectorAll('button');
 
 
